5a24e47c674defcf50fe322cfefebd116908b62c
Ryan Bigg <radarlistener@gmail.com>
2011-06-16 16:13:03 +1000

    Implement devise authenticator

--- lib/cassy/authenticators/devise.rb
+++ lib/cassy/authenticators/devise.rb
@@ -2,7 +2,10 @@ module Cassy
   module Authenticators
     class Devise < Base
       def self.validate(credentials)
-        p credentials
+        # Find the user with the given email
+        user = User.find_by_email(credentials[:username])
+        # Did we find a user, and is their password valid?
+        user && user.valid_password?(credentials[:password])
       end
     end
   end
