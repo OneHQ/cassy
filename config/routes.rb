3e7bf167a8659e626ea719e9fb040cfd180515d1
Ryan Bigg <radarlistener@gmail.com>
2011-06-16 16:49:37 +1000

    Cassy.draw_old_routes is now Cassy.draw_routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 # When this is used in a Rails 3.1 application, use these.
-# Until then: Cassy.draw_old_routes
+# Until then: Cassy.draw_routes
 # Cassy::Engine.routes.draw do
 #   get 'login', :to => "sessions#new"
 #   post 'login', :to => "sessions#create"
--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,5 +1,5 @@
 # Draws Rails 3.0 routes
-Cassy.draw_old_routes
+Cassy.draw_routes
 
 # Placeholder for dummy routes
 Rails.application.routes.draw do
