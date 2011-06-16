9dd69e7fd9f94d73244bc8ae7f05de685c9557a8
Ryan Bigg <radarlistener@gmail.com>
2011-06-16 18:30:57 +1000

    Just settings, not settings.config

--- lib/cassy/cas.rb
+++ lib/cassy/cas.rb
@@ -147,7 +147,7 @@ module Cassy
         error = "No ticket granting ticket given."
         logger.debug error
       elsif tgt = TicketGrantingTicket.find_by_ticket(ticket)
-        if settings.config[:maximum_session_lifetime] && Time.now - tgt.created_on > settings.config[:maximum_session_lifetime]
+        if settings[:maximum_session_lifetime] && Time.now - tgt.created_on > settings[:maximum_session_lifetime]
   	tgt.destroy
           error = "Your session has expired. Please log in again."
           logger.info "Ticket granting ticket '#{ticket}' for user '#{tgt.username}' expired."
