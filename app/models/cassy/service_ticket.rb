49dd7f6572c8b3efc983d849a6d6c0f8675db0eb
Mikel Lindsaar <mikel@rubyx.com>
2012-02-02 16:36:05 +1100

    Manually inserting type into type column.
    Rails 3.2.1 required that we change Ticket to a module and include it
    instead of it being an abstract_class.  This then caused the problem
    where the service_ticket class was not inserting the class name into
    the type column, which was raising a PG exception due to the table
    having a not null constraint on this column.
    
    We might not need this column at all, but need tests to confirm that
    we don't need it.

--- app/models/cassy/service_ticket.rb
+++ app/models/cassy/service_ticket.rb
@@ -1,7 +1,20 @@
 module Cassy
   class ServiceTicket < ActiveRecord::Base
 	include Cassy::Ticket
+
     set_table_name 'casserver_st'
+
+    # Need to confirm the before_save function is actually needed. Seems that every
+    # instance of this type (in 130000 rows) has the same value of "Cassy::ServiceTicket"
+    # but before I nuke it, need to include tests.  This error came up after migrating
+    # from Rails 3.1 to 3.2, inheritence columns are not auto filled unless the parent
+    # model is not an abstract class, see commit e425aec66acc909e68992616fb2bfc4ce6d2f629
+    # where the Ticket model was changed to a module.
+    before_save :set_type_column
+    def set_type_column
+      self.type = "Cassy::ServiceTicket"
+    end
+
     include Consumable
 
     belongs_to :granted_by_tgt, :class_name => 'Cassy::TicketGrantingTicket', :foreign_key => :granted_by_tgt_id
