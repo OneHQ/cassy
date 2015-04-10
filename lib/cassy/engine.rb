9b7d83c876ae30f1730899bef005c73678f49842
Josh Bodah <jb3689@yahoo.com>
2015-04-10 14:44:07 -0400

    allow for ERB in cassy.yml

--- lib/cassy/engine.rb
+++ lib/cassy/engine.rb
@@ -1,8 +1,9 @@
 module Cassy
   class Engine < Rails::Engine
     config.config_file = ENV["RUBYCAS_CONFIG_FILE"]
-    config.after_initialize do 
-      config.configuration = HashWithIndifferentAccess.new(YAML.load_file(config.config_file))
+    config.after_initialize do
+      file = File.read(config.config_file)
+      config.configuration = HashWithIndifferentAccess.new(YAML.load(ERB.new(file).result))
     end
   end
 end
